6bda27a4c5756493e4ae5c625ac161a9766c9438
PolyHodl <polyhodl@protonmail.com>
2021-05-04 13:27:45 -0400

    inital resources

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,51 @@
+export const links = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x57067A6BD75c0E95a6A5f158455926e43E79BeB0',
+        external: true,
+      },
+    ],
+  },
+  {
+    label: 'Github',
+    icon: 'GithubIcon',
+    href: 'https://github.com/polyhodl',
+    external: true,
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "#",
+      },
+      {
+        label: "Announcements",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "#",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
